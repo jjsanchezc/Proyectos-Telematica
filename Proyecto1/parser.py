e022b1560dd49cf9535e99ce860fd1676bb90b0f
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-10-27 00:05:48 -0500

    añadimos el parser.py

---
+++ Proyecto1/parser.py
@@ -0,0 +1,14 @@
+from configparser import ConfigParser
+
+config = ConfigParser()
+
+config["DEFAULT"] = {
+    "port" : 8080,
+    "max_conn": 5,
+    "buffer_size": 8192,
+    "ttl":180,
+    "ip_server": ['34.204.107.148', '3.92.231.50', '44.201.131.175']
+}
+
+with open("parser.ini","w") as f:
+    config.write(f)
